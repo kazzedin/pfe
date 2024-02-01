86de6c885f3493c712a8665ee14307ba3f6f1b19
koudid <koudid@azzedin29>
2024-02-01 15:32:46 +0100

    mettre a jour le fichier server.js + ajouter des dependencies

---
+++ back/server.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app = express();
+const cors=require('cors');
+require('dotenv').config();
+const port=process.env.PORT
+app.use(express.json());
+app.use(cors());
+
+app.listen(port,()=>{
+    console.log(`listening on port http://localhost:${port}`);
+})
